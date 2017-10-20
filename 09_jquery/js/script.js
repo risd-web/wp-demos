85a31cbbca911db8b761af1cb897e775c3c52fc3
Marie Otsuka <marieotsuka@users.noreply.github.com>
2017-10-20 16:34:46 -0400

    jquery demos

--- 09_jquery/js/script.js
+++ 09_jquery/js/script.js
@@ -1,28 +1,19 @@
-console.log("hello from external script.js"); //just to check that your script is loading in console
+console.log("some text here"); //just to check that your script is loading in console
  	
-	var x = 4;
-	console.log(x);
-	x = x + 1;
-	console.log(x + " after x=x+1");
-
 
 $(document).ready(function(){
 
-	$(".button").click(function(){
 
+	$(".button").click(function(){
 		console.log("check for button click");
-		$("h1").toggleClass("reveal");
+		$("h1").toggleClass('reveal');
 	});
 
 
 	$(".element").click(function(){
-		$(this).toggleClass("play");
-		$('h1').text(myText);
+		console.log("clicking element");
+		$(this).toggleClass('play');
 	});
-
-	var myText = $('h2').text();
-
-	console.log(myText);
 
 
 });
